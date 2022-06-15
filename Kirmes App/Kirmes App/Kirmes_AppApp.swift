f34a24398a1c354a7edc1d648505b3dd38b7c9f2
AlexanderBrauneck <73655966+AlexanderBrauneck@users.noreply.github.com>
2022-06-15 17:01:47 +0200

    ProjectStart

---
+++ Kirmes App/Kirmes App/Kirmes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kirmes_AppApp.swift
+//  Kirmes App
+//
+//  Created by Anna Reyhe on 15.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Kirmes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

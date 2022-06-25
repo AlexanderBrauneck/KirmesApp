ed80b60dfe4f0e450f2101bd42ecabbe900047f3
AlexanderBrauneck <73655966+AlexanderBrauneck@users.noreply.github.com>
2022-06-25 13:50:33 +0200

    UI Start

--- Kirmes App/Kirmes App/ContentView.swift
+++ Kirmes App/Kirmes App/ContentView.swift
@@ -9,13 +9,74 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ItemView()
+            ItemView()
+            ItemView()
+            ItemView()
+            ItemView()
+            Spacer()
+        }
     }
 }
+
+struct ItemView: View {
+    @State var itemCount: Int = 0
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            shape.fill().foregroundColor(.yellow)
+            HStack {
+                remove
+                Spacer()
+                Text("Bier")
+                Text("\(itemCount)")
+                Spacer()
+                add
+            }
+        }
+    }
+    
+    var remove: some View {
+        Button(action: { if itemCount >= 1 { itemCount -= 1 } }, label: {Image(systemName: "minus.circle")})
+    }
+    
+    var add: some View {
+        Button(action: { itemCount += 1 }, label: {Image(systemName: "plus.circle")})
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
